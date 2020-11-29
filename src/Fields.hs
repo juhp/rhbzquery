7616ccea432aa0bdc7c8f2de7a5c118f8259b0a8
Jens Petersen <petersen@redhat.com>
2020-11-29 18:50:50 +0800

    support flag= (and flags=?)

--- src/Fields.hs
+++ src/Fields.hs
@@ -14,7 +14,6 @@ data BzFields = BzProduct
               | BzComponent
               | BzStatus
               | BzParameter String
---              | BzFlag String
               | BzMeta Char Natural
   deriving Eq
 
@@ -24,7 +23,6 @@ instance Show BzFields where
   show BzComponent = "component"
   show BzStatus = "bug_status"
   show (BzParameter f) = mapFields f
-  -- showField (BzFlag f) =
   show (BzMeta c n) = c: show n
 
 mapFields :: String -> String
@@ -33,15 +31,30 @@ mapFields "itr" = "cf_internal_target_release"
 mapFields "verified" = "cf_verified"
 mapFields s = s
 
+mapComplex :: String -> (String,String)
+mapComplex "sst" = ("agile_team.name","equals")
+mapComplex "flag" = ("flagtypes.name","substr")
+mapComplex "flags" = ("flagtypes.name","substr")
+mapComplex p = (p,"equals")
+
+-- FIXME support sst=name (map sst=name to sst_name)?
 argToFields :: Natural -> ArgType -> (Natural,[(BzFields,String)])
 argToFields i arg =
   case arg of
     ArgProdVer prodver -> (i,productVersionQuery prodver)
-    ArgSST sst -> (i+1,[(BzMeta 'f' i,"agile_team.name")
-                       ,(BzMeta 'o' i,"equals")
-                       ,(BzMeta 'v' i,sst)])
+    ArgSST sst ->
+      let (p,o) = mapComplex "sst"
+      in (i+1,[(BzMeta 'f' i, p)
+              ,(BzMeta 'o' i, o)
+              ,(BzMeta 'v' i, sst)])
     ArgStatus st -> (i,[(BzStatus,st)])
-    ArgParameter p v -> (i,[(BzParameter p, v)])
+    ArgParameter param v ->
+      let (p,o) = mapComplex param
+      in if '.' `elem` p
+         then (i+1,[(BzMeta 'f' i, p)
+                   ,(BzMeta 'o' i, o)
+                   ,(BzMeta 'v' i, v)])
+         else (i,[(BzParameter p, v)])
     ArgOther c -> (i,[(BzComponent,c)])
 
 productVersionQuery :: ProductVersion -> [(BzFields,String)]
