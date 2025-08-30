c5742333cd6cd7fa0edd5a303127ddcbe70778c2
Jens Petersen <juhpetersen@gmail.com>
2025-08-30 21:55:21 +0800

    drop legacy ansi-wl-pprint with P.pretty

--- src/Help.hs
+++ src/Help.hs
@@ -9,19 +9,18 @@ import Data.List.Extra
 #if !MIN_VERSION_base(4,11,0)
 import Data.Monoid ((<>))
 #endif
-import qualified Text.PrettyPrint.ANSI.Leijen as P
+import qualified Options.Applicative.Help.Pretty as P
 
 import ParseArg (statusList)
 
 detailedHelp :: P.Doc
 detailedHelp =
   P.vcat
-  [ P.empty
-  , P.text "Tool for generating bugzilla queries"
-  , P.empty
-  , P.text "STATUS = " <> P.lbrace <> P.align (P.fillCat (P.punctuate P.comma (map P.text (map lower statusList ++ ["all (open and closed)", "'<STATE'", "'>STATE'"]))) <> P.rbrace)
-  , P.text "PRODUCTVERSION = " <> P.lbrace <> P.align (P.fillCat (P.punctuate P.comma (map P.text ["rawhide", "fedora", "fXY", "epel", "epelX", "rhel8", "rhel7", "rhelX.Z"])) <> P.rbrace)
-  , P.text "op = search operator (eg '~' for substring: \"summary~akeyword\")"
-  , P.empty
-  , P.text "See https://github.com/juhp/rhbzquery#readme for examples"
+  [ P.pretty "Tool for generating bugzilla queries"
+  , P.pretty ""
+  , P.pretty "STATUS = " <> P.lbrace <> P.align (P.fillCat (P.punctuate P.comma (map P.pretty (map lower statusList ++ ["all (open and closed)", "'<STATE'", "'>STATE'"]))) <> P.rbrace)
+  , P.pretty "PRODUCTVERSION = " <> P.lbrace <> P.align (P.fillCat (P.punctuate P.comma (map P.pretty ["rawhide", "fedora", "fXY", "epel", "epelX", "rhel8", "rhel7", "rhelX.Z"])) <> P.rbrace)
+  , P.pretty "op = search operator (eg '~' for substring: \"summary~akeyword\")"
+  , P.pretty ""
+  , P.pretty "See https://github.com/juhp/rhbzquery#readme for examples"
   ]
